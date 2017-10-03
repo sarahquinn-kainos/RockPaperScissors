be498d9ed8e236a221113acaa1ab9b0222bb1f61
sarah <sarahq@kainos.com>
2017-10-03 10:32:28 +0100

    PLayer class created

---
+++ src/com/company/Player.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Player {
+    public String userName;
+    public int currentWins;
+    public int currentLosses;
+
+    public Player(String userName, int currentWins, int currentLosses) {
+        this.userName = userName;
+        this.currentWins = 0;
+        this.currentLosses = 0;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getCurrentWins() {
+        return currentWins;
+    }
+
+    public void setCurrentWins(int currentWins) {
+        this.currentWins = currentWins;
+    }
+
+    public int getCurrentLosses() {
+        return currentLosses;
+    }
+
+    public void setCurrentLosses(int currentLosses) {
+        this.currentLosses = currentLosses;
+    }
+}
+
+
