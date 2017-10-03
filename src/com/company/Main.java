ddf6d362ddd8af9678ebddb5d6efed9a2945d8fb
Michael Dunwoody <michaeld@kainos.com>
2017-10-03 10:32:34 +0100

    Started branch with main method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    boolean firstGame = true;
+	System.out.println("Hello and welcome to our game of rock paper scissors");
+	SetupGame(firstGame);
+    }
+
+    public static void SetupGame(boolean isFirst)
+    {
+        if (isFirst) {
+            //Go straight to create player
+        }
+        else {
+            //ShowMenu();
+        }
     }
 }
