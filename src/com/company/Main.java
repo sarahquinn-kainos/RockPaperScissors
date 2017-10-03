60cefdc3daf4d2a5a4b1f60df032cea1f062bf4b
Michael Dunwoody <michaeld@kainos.com>
2017-10-03 12:27:06 +0100

    Fixing validation with player objects. Committed before taking lunch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -37,9 +37,14 @@ public class Main {
         System.out.println("Please input your desired username");
         Scanner usernameScanner = new Scanner(System.in);
         String username = usernameScanner.nextLine();
+        for (Player player :
+                players) {
+            if (player.userName)
+            
+        }
         Player player = new Player(username,0,0);
         players.add(player);
-        //menu();
+        //can't use taken username
     }
 
     public static void menu() {
@@ -55,12 +60,12 @@ public class Main {
         switch (menuSelect) {
             case "1":
                 // run create player function
-                System.out.println("Player Created");
+                CreatePlayer();
                 Main.menu();
                 break;
             case "2":
                 // run select player function
-                System.out.println("Player Selection Ran");
+                selectPlayer1();
                 Main.menu();
                 break;
             case "3":
@@ -123,7 +128,7 @@ public class Main {
         if (players.get(selection) != null){
             if (!players.get(selection).userName.equals( player1.userName)){
                 player2 = players.get(selection);
-
+                startGame(player1,player2);
             }
             else {
                 System.out.println("Error. You can't play against yourself! get a friend");
